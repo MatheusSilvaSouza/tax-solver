aa6b23271f22ef8bab7d8c67c76579696914b028
MatheusSilvaSouza <matheussilvadsouza@gmail.com>
2023-02-18 15:59:58 -0300

    Use Class to solve TaxSolver

---
+++ README.md
@@ -0,0 +1,22 @@
+Ruby Version: 2.7.7p221
+
+Run using interactive ruby console
+```
+irb -r ./tax_solver.rb
+```
+
+For each input, create an instance of the class `TaxSolver` and call the method `solve`
+```
+TaxSolver.new.solve
+```
+
+After that, the program will be waiting for the input. Is assumed that the input ends after an empty line
+
+```
+2 book at 12.49
+1 music CD at 14.99
+1 chocolate bar at 0.85
+
+```
+
+The output will be displayed in the console
--- tax_solver.rb
+++ tax_solver.rb
@@ -1,35 +1,39 @@
-def taxable_product?(product_name)
-  !product_name.match(/book|chocolate|pills/)
+class TaxSolver
+  def solve()
+    input = []
+    while (line = gets) != "\n"
+      input << line
+    end
+    sale_taxes = 0
+    total = 0
+    input.map do |inp|
+      amount = inp.split(" ").first.to_i
+      product_name = inp.split(" at ").first.strip
+      price = inp.split(" at ").last.strip.to_f
+      tax = 0
+      tax += 0.1 if taxable_product?(product_name)
+      tax += 0.05 if apply_import_tax?(product_name)
+
+      price_amount = amount * price
+      tax_per_unit = ((price * tax) * 20).ceil / 20.0
+      tax_value = amount * tax_per_unit
+      product_total = price_amount + tax_value
+
+      puts "#{product_name}: %.2f" % product_total
+
+      sale_taxes += tax_value
+      total += product_total
+    end
+
+    puts "Sales Taxes: %.2f" % sale_taxes
+    puts "Total: %.2f" % total
+  end
+
+  def taxable_product?(product_name)
+    !product_name.match(/book|chocolate|pills/)
+  end
+
+  def apply_import_tax?(product_name)
+    product_name.match(/imported/)
+  end
 end
-
-def apply_import_tax?(product_name)
-  product_name.match(/imported/)
-end
-
-input = []
-while (line = gets) != "\n"
-  input << line
-end
-sale_taxes = 0
-total = 0
-input.map do |inp|
-  amount = inp.split(" ").first.to_i
-  product_name = inp.split(" at ").first.strip
-  price = inp.split(" at ").last.strip.to_f
-  tax = 0
-  tax += 0.1 if taxable_product?(product_name)
-  tax += 0.05 if apply_import_tax?(product_name)
-
-  price_amount = amount * price
-  tax_per_unit = ((price * tax) * 20).ceil / 20.0
-  tax_value = amount * tax_per_unit
-  product_total = price_amount + tax_value
-
-  puts "#{product_name}: %.2f" % product_total
-
-  sale_taxes += tax_value
-  total += product_total
-end
-
-puts "Sales Taxes: %.2f" % sale_taxes
-puts "Total: %.2f" % total
